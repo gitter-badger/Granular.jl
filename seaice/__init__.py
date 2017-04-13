102f9654499c4b347997185effd4936b7e9a3faf
Anders Damsgaard <andersd@riseup.net>
2017-04-13 10:58:09 -0400

    add package docstring

--- seaice/__init__.py
+++ seaice/__init__.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
+'''
+SeaIce
+
+:Package name: seaice
+:Release date: 2017-04-13
+:Authors: Anders Damsgaard:
+:URL: https://github.com/anders-dc/seaice
+:License: GPLv3
+'''
+from __future__ import absolute_import
+
+from .icefloe import IceFloeCylindrical
+# from packing import *
 
 __version__ = '0.1b'
 __author__ = 'Anders Damsgaard <anders.damsgaard@noaa.gov>'
 
-from icefloe import *
-from packing import *
+__all__ = ['IceFloeCylindrical']
