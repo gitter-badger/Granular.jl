7738dd99c78168b534c9b206f1a84efdf2f9f112
Anders Damsgaard <andersd@riseup.net>
2017-04-21 16:37:54 -0400

    move MOM6 netcdf read to ocean.jl

--- test/netcdf.jl
+++ test/netcdf.jl
@@ -8,7 +8,7 @@ import SeaIce
 info("#### $(basename(@__FILE__)) ####")
 
 info("Testing dimensions of content read from prog__0001_006.nc")
-ocean = SeaIce.readNetCDF("prog__0001_006.nc")
+ocean = SeaIce.readOceanNetCDF("prog__0001_006.nc")
 @test length(ocean.xq) == 44
 @test length(ocean.xh) == 44
 @test length(ocean.yq) == 40
