ddb4611e8e4fa17f9e08d61a5b5d16ce25bf00e1
Anders Damsgaard <andersd@riseup.net>
2017-04-25 15:57:06 -0400

    cross-platform compatibility for checksum verification

--- test/vtk.jl
+++ test/vtk.jl
@@ -9,6 +9,13 @@ sim = SeaIce.createSimulation(id="test")
 SeaIce.addIceFloeCylindrical(sim, [ 0., 0.], 10., 1., verbose=false)
 SeaIce.addIceFloeCylindrical(sim, [18., 0.], 10., 1., verbose=false)
 SeaIce.writeVTK(sim)
-checksum = readstring(`sha256sum test.1.vtu`)
+
+if Base.is_linux()
+    checksum = readstring(`sha256sum test.1.vtu`)
+elseif Base.is_apple()
+    checksum = readstring(`shasum -a 256 test.1.vtu`)
+else
+    warn("checksum verification of VTK file not supported on this platform")
+end
 rm("test.1.vtu")
 @test checksum == "1c0c2bdd265abdda22ef3727e7cac829e2321462d494be2e23364653f9529c87  test.1.vtu\n"
