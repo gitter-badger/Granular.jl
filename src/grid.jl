66d9f3e3d75e83a0d4550bdfeb7f7e7f127cd341
Anders Damsgaard <andersd@riseup.net>
2017-05-01 16:15:00 -0400

    add fix for ice floe disabling

--- src/grid.jl
+++ src/grid.jl
@@ -53,6 +53,7 @@ function sortIceFloesInOceanGrid!(simulation::Simulation; verbose=false)
         # remove ice floe if it is outside of the grid
         if i == 0 && j == 0
             disableIceFloe!(simulation, idx)
+            continue
         end
 
         # add cell to ice floe
