ca65d2ddfa993e5a21e7a184d94787a4a6d7350b
Anders Damsgaard <andersd@riseup.net>
2017-05-01 22:33:20 -0400

    add escaped symbol to end command sent to powershell

--- test/vtk.jl
+++ test/vtk.jl
@@ -11,16 +11,22 @@ SeaIce.addIceFloeCylindrical(sim, [18., 0.], 10., 1., verbose=false)
 sim.ocean = SeaIce.createRegularOceanGrid([10, 20, 5], [10., 25., 2.])  
 SeaIce.writeVTK(sim, verbose=false)
 
+cmd_post = ""
 if Base.is_linux()
     cmd = "sha256sum"
 elseif Base.is_apple()
     cmd = ["shasum", "-a", "256"]
 elseif Base.is_windows()
-    cmd = ["powershell", "-Command", "\"Get-FileHash", "-Algorithm", "SHA256\""]
+    cmd = ["powershell", "-Command", "\"Get-FileHash", "-Algorithm", "SHA256"]
+    cmd_post = "\""
 else
     error("checksum verification of VTK file not supported on this platform")
 end
-@test readstring(`$(cmd) test.icefloes.1.vtu`) == "72f4e4b854d7e92afd8cde0b79a4af6a29e49714b751ffc30a4ff3867f44b505  test.icefloes.1.vtu\n"
-@test readstring(`$(cmd) test.ocean.1.vts`) == "f0117e414c4e71a0c55980f63865eb03b6c597fa2546983258b8a57eb4ff2a25  test.ocean.1.vts\n"
+
+@test readstring(`$(cmd) test.icefloes.1.vtu$(cmd_post)`) == 
+"72f4e4b854d7e92afd8cde0b79a4af6a29e49714b751ffc30a4ff3867f44b505  test.icefloes.1.vtu\n"
+
+@test readstring(`$(cmd) test.ocean.1.vts$(cmd_post)`) == 
+"f0117e414c4e71a0c55980f63865eb03b6c597fa2546983258b8a57eb4ff2a25  test.ocean.1.vts\n"
 
 SeaIce.removeSimulationFiles(sim)
