c819d60acdf7a52037a3d029fdc579ea3218993e
Anders Damsgaard <andersd@riseup.net>
2017-07-11 09:30:35 +0200

    test memory usage over the course of a collision

--- test/memory-management.jl
+++ test/memory-management.jl
@@ -47,6 +47,7 @@ sim = SeaIce.createSimulation()
 @test Base.summarysize(sim) == empty_sim_size_recursive
 
 info("Check memory usage when stepping time for empty simulation object")
+sim = SeaIce.createSimulation()
 sim.time_step = 1.0
 for i=1:10
     SeaIce.run!(sim, single_step=true, verbose=false)
@@ -92,3 +93,46 @@ for i=1:10
         size_per_icefloe_recursive*length(sim.ice_floes)
 end
 
+info("Check memory when stepping time with two interacting ice floes (cell sorting)")
+sim = SeaIce.createSimulation()
+SeaIce.addIceFloeCylindrical!(sim, [1., 1.], 1., 1., verbose=false)
+SeaIce.addIceFloeCylindrical!(sim, [1., 1.], 1.9, 1., verbose=false)
+nx, ny, nz = 5, 5, 1
+sim.ocean = SeaIce.createRegularOceanGrid([nx, ny, nz], [10., 10., 10.])
+sim.time_step = 1e-6
+SeaIce.run!(sim, single_step=true, verbose=false)
+original_size_recursive = Base.summarysize(sim)
+for i=1:10
+    SeaIce.run!(sim, single_step=true, verbose=false)
+    @test Base.summarysize(sim) == original_size_recursive
+end
+
+info("Checking if memory is freed after ended collision (all to all)")
+sim = SeaIce.createSimulation(id="test")
+SeaIce.addIceFloeCylindrical!(sim, [0., 0.], 10., 1., verbose=verbose)
+SeaIce.addIceFloeCylindrical!(sim, [20.05, 0.], 10., 1., verbose=verbose)
+sim.ice_floes[1].lin_vel[1] = 0.1
+SeaIce.setTotalTime!(sim, 10.0)
+SeaIce.setTimeStep!(sim, epsilon=0.07)
+original_size_recursive = Base.summarysize(sim)
+SeaIce.run!(sim, verbose=false)
+@test Base.summarysize(sim) == original_size_recursive
+
+info("Checking if memory is freed after ended collision (cell sorting)")
+sim = SeaIce.createSimulation(id="test")
+SeaIce.addIceFloeCylindrical!(sim, [0., 0.], 10., 1., verbose=verbose)
+SeaIce.addIceFloeCylindrical!(sim, [20.05, 0.], 10., 1., verbose=verbose)
+sim.ocean = SeaIce.createRegularOceanGrid([2, 2, 2], [40., 40., 10.])
+sim.ice_floes[1].lin_vel[1] = 0.1
+SeaIce.setTotalTime!(sim, 10.0)
+SeaIce.setTimeStep!(sim, epsilon=0.07)
+SeaIce.run!(sim, single_step=true, verbose=false)
+original_sim_size_recursive = Base.summarysize(sim)
+original_icefloes_size_recursive = Base.summarysize(sim.ice_floes)
+original_ocean_size_recursive = Base.summarysize(sim.ocean)
+original_atmosphere_size_recursive = Base.summarysize(sim.atmosphere)
+SeaIce.run!(sim, verbose=false)
+@test Base.summarysize(sim.ice_floes) == original_icefloes_size_recursive
+@test Base.summarysize(sim.ocean) == original_ocean_size_recursive
+@test Base.summarysize(sim.atmosphere) == original_atmosphere_size_recursive
+@test Base.summarysize(sim) == original_sim_size_recursive
