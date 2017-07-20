aaf8bb42d7159bbf0966407c4f8d263cf227396f
Anders Damsgaard <andersd@riseup.net>
2017-07-20 12:06:27 +0200

    add test for lower range of power-law RNG

--- test/util.jl
+++ test/util.jl
@@ -20,6 +20,7 @@ for i=1:10^5
     @test 0. <= SeaIce.randpower()[1] <= 1.
     @test 0. <= SeaIce.randpower(1, 1., 0., 1.)[1] <= 1.
     @test 0. <= SeaIce.randpower(1, 1., 0., .1)[1] <= .1
+    @test 5. <= SeaIce.randpower(1, 1., 5., 6.)[1] <= 6.
     @test 0. <= minimum(SeaIce.randpower((2,2), 1., 0., 1.))
     @test 1. >= maximum(SeaIce.randpower((2,2), 1., 0., 1.))
     @test 0. <= minimum(SeaIce.randpower(5, 1., 0., 1.))
