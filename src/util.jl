04f4bab6f3c7c384788ca1f02ca9732d91232f33
Anders Damsgaard <andersd@riseup.net>
2017-07-20 12:23:55 +0200

    return random value as Float64 when a single value is requested

--- src/util.jl
+++ src/util.jl
@@ -18,7 +18,13 @@ Returns one or more random numbers from a power-law probability distribution.
                            min_val::Number = 0.,
                            max_val::Number = 1.)
 
-    return ((max_val^(distribution_power + 1.) - 
-             min_val^(distribution_power + 1.))*Base.Random.rand(dims) + 
-            min_val^(distribution_power + 1.)).^(1./(distribution_power + 1.))
+    val = ((max_val^(distribution_power + 1.) - 
+            min_val^(distribution_power + 1.))*Base.Random.rand(dims) + 
+           min_val^(distribution_power + 1.)).^(1./(distribution_power + 1.))
+
+    if dims == 1
+        return val[1]
+    else
+        return val
+    end
 end
