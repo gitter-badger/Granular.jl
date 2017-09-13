a19e2a8c201bda48eafc46a2080e19f0a532c429
Anders Damsgaard <andersd@riseup.net>
2017-09-12 22:26:09 -0400

    trim whitespace around output

--- examples/logo.animate.sh
+++ examples/logo.animate.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-convert -delay 10 -transparent-color white -reverse -loop 0 logo/*.png logo/logo.gif
+convert -trim +repage \
+    -delay 10 -transparent-color white -reverse -loop 0 \
+    logo/*.png logo/logo.gif
