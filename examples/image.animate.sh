eed01ce4f62b8be47673fc2aac87ce425dda2fe8
Anders Damsgaard <andersd@riseup.net>
2017-11-01 11:35:54 -0400

    remove redundant legacy render and animation scripts

--- examples/image.animate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-convert -trim +repage \
-    -delay 10 -transparent-color white -reverse -loop 0 \
-    image/*.png image/image.gif
-
-convert -trim +repage \
-    -delay 10 -transparent-color white -loop 0 \
-    image/*.png image/image-forward.gif
--- examples/logo.animate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-convert -trim +repage \
-    -delay 10 -transparent-color white -reverse -loop 0 \
-    logo/*.png logo/logo.gif
