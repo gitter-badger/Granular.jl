24c69b8beed23fe0a2fbbc13a1b60d7686dd1011
Anders Damsgaard <andersd@riseup.net>
2017-11-07 13:06:54 -0500

    update BinDeps script with Linux and Windows instructions

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,27 @@
 #!/usr/bin/env julia
 using BinDeps
+using Compat
 
 @BinDeps.setup
 
-imagemagick = library_dependency("imagemagick")
+imagemagick = library_dependency("imagemagick", aliases = ["ImageMagick"])
 
-if is_apple()
+if is_linux()
+    provides(AptGet, "imagemagick", imagemagick, os = :Linux)
+    provides(Yum, "ImageMagick", imagemagick, os = :Linux)
+    provides(Pacman, "imagemagick", imagemagick, os = :Linux)
+
+elseif is_apple()
+    if Pkg.installed("Homebrew") === nothing
+        error("Homebrew julia package not installed, " *
+              "please run Pkg.add(\"Homebrew\")")
+    end
     using Homebrew
     provides(Homebrew.HB, "imagemagick", imagemagick, os = :Darwin)
+
+elseif is_windows()
+    using WinRPM
+    provides(WinRPM.RPM, "imagemagick", imagemagick, os = :Windows)
 end
 
 @BinDeps.install Dict(:imagemagick => :imagemagick)
